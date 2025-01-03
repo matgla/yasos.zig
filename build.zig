860f94c4e442a338dfc06cbd18d5aa72c1196005
Mateusz Stadnik <matgla@live.com>
2025-01-03 21:11:36 +0100

    still working on kconfig

--- build.zig
+++ build.zig
@@ -1,12 +1,23 @@
 const std = @import("std");
 const hal = @import("yasos_hal");
 
+pub fn configure_kconfig(b: *std.Build) void {
+    const python = b.findProgram(&.{"python3"}, &.{}) catch {
+        std.log.err("Can't find python3 in system path", .{});
+        unreachable;
+    };
+    _ = b.run(&.{ python, "-m", "venv", "yasos_venv" });
+    _ = b.run(&.{ "./yasos_venv/bin/pip", "install", "-r", "./kconfiglib/requirements.txt" });
+    _ = b.run(&.{ "./yasos_venv/bin/python", "-m", "menuconfig", "KConfig" });
+}
+
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     const board = b.option([]const u8, "board", "a board for which the HAL is used") orelse "host";
     const cmake = b.option([]const u8, "cmake", "path to CMake executable") orelse "";
     const gcc = b.option([]const u8, "gcc", "path to arm-none-eabi-gcc executable") orelse "";
 
+    configure_kconfig(b);
     const boardDep = b.dependency("yasos_hal", .{
         .board = board,
         .root_file = @as([]const u8, b.pathFromRoot("source/main.zig")),
