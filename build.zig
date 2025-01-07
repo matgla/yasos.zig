1b167b6a50d1aae909dde19ee630195deedc5181
Mateusz Stadnik <matgla@live.com>
2025-01-07 21:41:24 +0100

    KConfig generator is working

--- build.zig
+++ build.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 const hal = @import("yasos_hal");
 
+var cmake: ?[]const u8 = null;
+var gcc: ?[]const u8 = null;
+
+fn prepare_venv(b: *std.Build) *std.Build.Step.Run {
+    const create_venv_args = [_][]const u8{ "python", "-m", "venv", "yasos_venv" };
+    const create_venv_command = b.addSystemCommand(&create_venv_args);
+    const install_requirements_args = [_][]const u8{ "./yasos_venv/bin/pip", "install", "-r", "kconfiglib/requirements.txt" };
+    const install_requirements_args_command = b.addSystemCommand(&install_requirements_args);
+    install_requirements_args_command.step.dependOn(&create_venv_command.step);
+    return install_requirements_args_command;
+}
+
 fn configure_kconfig(b: *std.Build) *std.Build.Step.Run {
     const argv = [_][]const u8{ "./yasos_venv/bin/python", "-m", "menuconfig", "KConfig" };
     const command = b.addSystemCommand(&argv);
@@ -8,7 +20,7 @@ fn configure_kconfig(b: *std.Build) *std.Build.Step.Run {
 }
 
 fn generate_config(b: *std.Build) *std.Build.Step.Run {
-    const argv = [_][]const u8{ "./yasos_venv/bin/python", "./kconfiglib/generate.py", "--input", ".config", "-k", "KConfig", "-o", "config.json" };
+    const argv = [_][]const u8{ "./yasos_venv/bin/python", "./kconfiglib/generate.py", "--input", ".config", "-k", "KConfig", "-o", "config/config.json" };
     const command = b.addSystemCommand(&argv);
     return command;
 }
@@ -18,8 +30,9 @@ const Config = struct {
     cpu: []const u8,
 };
 
-fn load_config(b: *std.Build, config_file: std.Build.LazyPath) !Config {
-    const file = try std.fs.cwd().openFile(config_file.getPath(b), .{});
+fn load_config(b: *std.Build) !Config {
+    const config_directory = try std.fs.cwd().openDir("config", .{});
+    const file = try config_directory.openFile("config.json", .{ .mode = .read_only });
     defer file.close();
 
     const data = try file.readToEndAlloc(b.allocator, 4096);
@@ -34,45 +47,45 @@ fn load_config(b: *std.Build, config_file: std.Build.LazyPath) !Config {
     return parsed.value;
 }
 
-fn make_kernel() {
-    const boardDep = b.dependency("yasos_hal", .{
-        .board = @as([]const u8, config.board),
-        .root_file = @as([]const u8, b.pathFromRoot("source/main.zig")),
-        .optimize = optimize,
-        .name = @as([]const u8, "yasos_kernel"),
-        .cmake = cmake,
-        .gcc = gcc,
-    });
-
-
-}
-
 pub fn build(b: *std.Build) !void {
-    // const optimize = b.standardOptimizeOption(.{});
-    // const cmake = b.option([]const u8, "cmake", "path to CMake executable") orelse "";
-    // const gcc = b.option([]const u8, "gcc", "path to arm-none-eabi-gcc executable") orelse "";
+    cmake = b.option([]const u8, "cmake", "path to CMake executable") orelse "";
+    gcc = b.option([]const u8, "gcc", "path to arm-none-eabi-gcc executable") orelse "";
 
+    const venv = prepare_venv(b);
     const configure = configure_kconfig(b);
     const generate = generate_config(b);
+    configure.step.dependOn(&venv.step);
+    generate.step.dependOn(&venv.step);
 
     const menuconfig = b.step("menuconfig", "Execute menuconfig UI");
-    const build_kernel = b.step("build_kernel", "Compile Yasos Kernel");
-
-    //con.step.dependOn(&configure.step);
-    // menuconfig.dependOn(&generate.step);
     generate.step.dependOn(&configure.step);
-    _ = generate.addPrefixedOutputFileArg("-o", "config.json");
     menuconfig.dependOn(&generate.step);
-    b.default_step.dependOn(menuconfig);
     generate.has_side_effects = true;
+    const cwd = std.fs.cwd();
 
-    // const config = try load_config(b, config_file);
-    // std.log.info("Loading configuration for board: {s}", .{config.board});
+    const config_directory: std.fs.Dir = cwd.openDir("config", .{}) catch {
+        std.log.err("'config/config.json' not found. Please call 'zig build menuconfig' before compilation", .{});
+        return;
+    };
+    const config_exists = config_directory.statFile("config.json") catch {
+        std.log.err("'config/config.json' not found. Please call 'zig build menuconfig' before compilation", .{});
+        return;
+    };
 
-    // const config_directory = try std.fs.openDirAbsolute(b.pathFromRoot("."), .{});
-
-    // const config = @import("config/config.zig");
-
-    // b.installArtifact(boardDep.artifact("yasos_kernel"));
-    // _ = boardDep.module("board");
+    if (config_exists.kind == .file) {
+        const optimize = b.standardOptimizeOption(.{});
+        const config = try load_config(b);
+        const boardDep = b.dependency("yasos_hal", .{
+            .board = @as([]const u8, config.board),
+            .root_file = @as([]const u8, b.pathFromRoot("source/main.zig")),
+            .optimize = optimize,
+            .name = @as([]const u8, "yasos_kernel"),
+            .cmake = cmake.?,
+            .gcc = gcc.?,
+        });
+        b.installArtifact(boardDep.artifact("yasos_kernel"));
+        _ = boardDep.module("board");
+    } else {
+        std.log.err("'config/config.json' not found. Please call 'zig build menuconfig' before compilation", .{});
+    }
 }
