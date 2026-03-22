cd16a8340bbc9cf468aa8359bb50752de5895e30
Mateusz Stadnik <matgla@live.com>
2026-03-22 17:11:09 +0100

    removed struct

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -102,7 +102,6 @@ pwd
 cd ..
 mkdir -p rootfs/tmp
 cp $SCRIPT_DIR/hello_world.c rootfs/usr
-cp $SCRIPT_DIR/test_struct.c rootfs/usr
 cp $SCRIPT_DIR/hello_script.sh rootfs/usr
 
 mkdir -p rootfs/dev
